a72a49949bbe09f5a6ca4213af5505634e8b9e27
mschild91 <michael.schild@mail.de>
2023-09-02 15:48:34 +0200

    fixed a bug in the second fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,7 +77,9 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=51.233334&&lon=6.7833
   })
 
   .then(() => {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Düsseldorf, Deutschland&limit=1&appid=${apiKey}&units=metric`)
+    fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=Düsseldorf, Deutschland&limit=1&appid=${apiKey}&units=metric`
+    )
       .then((response) => {
         if (!response.ok) {
           throw new Error("Hier hat irgendwas nicht funktioniert.")
